dde1f415f3473ece99c8401dc2dc0e24d7507e41
stabldev <114811070+stabldev@users.noreply.github.com>
2026-03-30 12:39:11 +0530

    chore: initial commit

--- app/api/routes/main.py
+++ app/api/routes/main.py
@@ -4,5 +4,5 @@ from .auth import auth_router
 from .expense import expense_router
 
 api_router = APIRouter()
-api_router.include_router(auth_router)
-api_router.include_router(expense_router)
+api_router.include_router(auth_router, prefix="/auth")
+api_router.include_router(expense_router, prefix="/expenses")
